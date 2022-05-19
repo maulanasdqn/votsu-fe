f093b179fbe67c86d8f6fe214f7cc4409608a69c
Maulana Sodiqin <maulana@pmg.id>
2022-05-19 23:54:17 +0700

    Maulana Sodiqin | Add Some Litle Validations

---
+++ src/components/common/VtsButton.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+interface props {
+  color?: string
+  text?: string
+  isSmall?: boolean
+  isWithBorder?: boolean
+  isFull?: boolean
+}
+
+const props = withDefaults(defineProps<props>(), {
+  color: '',
+  text: '',
+  isSmall: false,
+  isWithBorder: false,
+  isFull: false,
+})
+</script>
+<template>
+  <button
+    class="dark:bg-gray-400 bg-blue-400 h-auto px-6 py-3 rounded-xl font-bold text-white shadow-lg hover:bg-blue-500 dark:hover:bg-gray-500 disabled:bg-gray-100 disabled:text-gray-300"
+    :class="
+      ({ 'px-4 py-3': isSmall },
+      { 'w-full': isFull },
+      {
+        'border-2 border-blue-400 dark:border-white bg-white dark:text-white dark:bg-gray-800 dark:text-gray-400 text-blue-400':
+          isWithBorder,
+      })
+    "
+  >
+    {{ text }}
+  </button>
+</template>
